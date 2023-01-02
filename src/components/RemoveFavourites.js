40709f05bdf4f93bc2d32b04007b626c300fe2c8
binarypsilocybin <53083287+binarypsilocybin@users.noreply.github.com>
2023-01-02 09:06:25 +0000

    1 file modified

--- src/components/RemoveFavourites.js
+++ src/components/RemoveFavourites.js
@@ -3,7 +3,7 @@ import React from 'react';
 const RemoveFavourites = () => {
   return (
     <>
-      <span className="mr-2">Remove from favourites</span>;
+      <span className="mr-2">Remove from favourites</span>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="16"
